37a189da71483c241bc57f84b54841a9bee347cc
dianagsgs <dianags@192.168.1.4>
2023-04-10 22:55:17 -0600

    meta thumbnail try again again

--- components/Layout/HeadContent.js
+++ components/Layout/HeadContent.js
@@ -28,7 +28,7 @@ const HeadContent = () => {
       <meta property="og:url" content="https://www.labestiaradio.com/" />
       <meta
         property="og:image"
-        content="/img/meta.png"
+        content="https://hola.somoslabestia.com/_next/image?url=%2Fimg%2Fhome%2Fmenu_items%2Fmenu-7.png&w=3840&q=75"
       />
       <meta property="og:type" content="website" />
       <meta name="twitter:card" content="player" />        
