3249190a4231b3cfd4624ac6fbb3ef2c81151c2f
dianagsgs <dianags@192.168.1.7>
2023-10-26 00:58:51 -0600

    implemented flask@

---
+++ backend/server.py
@@ -0,0 +1,40 @@
+from flask import Flask, Response
+
+import json
+
+api = Flask(__name__)
+
+@api.route('/get_locutores')
+def my_profile():
+
+    personas = [
+        {
+            "nombre":"Jen",
+            "programas": [
+                {"nombre":"Lunes Inexpertos","horario":"Lunes 11am"},
+                {"nombre":"Playlists Inexpertas","horario":"Viernes 11am"}
+            ],
+            "foto":"jen.png",
+            "bio":"Jen es chida"
+        },
+        {
+            "nombre":"Remofis",
+            "programas": [
+                {"nombre":"La Hora de la Bestia","horario":"Lunes a Viernes 10am"}
+            ],
+            "foto":"remofis.png",
+            "bio":"Es la jefa de la bestia"
+        },
+        {
+            "nombre":"Sandy",
+            "programas": [
+                {"nombre":"Supernova","horario":"Jueves 7pm"}
+            ],
+            "foto":"sandy.png",
+            "bio":"Sandy lleva desde el principio"
+        },
+    ]
+
+    resp = Response(response=json.dumps(personas), status=200, mimetype="text/plain")
+    resp.headers['Access-Control-Allow-Origin'] = 'http://localhost:3000'
+    return resp
--- pages/locutores.js
+++ pages/locutores.js
@@ -1,38 +1,14 @@
 import styles from "../styles/locutores.module.css";
-import { Fragment } from "react";
+import axios from "axios";
+
+import { Fragment, useEffect, useState } from "react";
 import Section from "../components/Layout/Section"
 import Menu from "../components/Layout/Menu";
 import CustomImage from "../components/UI/CustomImage";
 
 
 export default function Locutores(props) {
-  const personas = [
-    {
-      "nombre":"Jen",
-      "programas": [
-        {"nombre":"Lunes Inexpertos","horario":"Lunes 11am"},
-        {"nombre":"Playlists Inexpertas","horario":"Viernes 11am"}
-      ],
-      "foto":"jen.png",
-      "bio":"Jen es chida"
-    },
-    {
-      "nombre":"Remofis",
-      "programas": [
-        {"nombre":"La Hora de la Bestia","horario":"Lunes a Viernes 10am"}
-      ],
-      "foto":"remofis.png",
-      "bio":"Es la jefa de la bestia"
-    },
-    {
-      "nombre":"Sandy",
-      "programas": [
-        {"nombre":"Supernova","horario":"Jueves 7pm"}
-      ],
-      "foto":"sandy.png",
-      "bio":"Sandy lleva desde el principio"
-    },
-  ];
+  const [locutores, setLocutores] = useState(null);
 
   const getBulletPoints = (list) => {
     let points = []
@@ -46,36 +22,50 @@ export default function Locutores(props) {
     return points;
   }
 
-  const getLocutores = () => {
-    let items = []
-    for(let i = 0; i < personas.length; i++) {
-      let item =
-        <Section
-          id={"locutor_"+i}
-          titulo="/img/titulos/locutores.png"
-          title_width="110"
-        >
-          {personas[i].nombre + ": "}
-          {getBulletPoints(personas[i].programas)}
-          <CustomImage
-            resp_w="13vw"
-            src={"/img/locutores/"+personas[i].foto}
-            w="83"
-            h="75"
-            id={"foto"+i}
-            // class={styles.player}
-          />
-          {personas[i].bio}
-        </Section>;
-      items.push(item);
-    }
-    return items;
-  }
+  useEffect(() => {
+    axios({
+      method: "GET",
+      url:"http://localhost:5000/get_locutores",
+    })
+    .then((response) => {
+      const personas = response.data;
+      let items = []
+      for(let i = 0; i < personas.length; i++) {
+        let item =
+          <Section
+            id={"locutor_"+i}
+            titulo="/img/titulos/locutores.png"
+            title_width="110"
+          >
+            {personas[i].nombre + ": "}
+            {getBulletPoints(personas[i].programas)}
+            <CustomImage
+              resp_w="13vw"
+              src={"/img/locutores/"+personas[i].foto}
+              w="83"
+              h="75"
+              id={"foto"+i}
+              // class={styles.player}
+            />
+            {personas[i].bio}
+          </Section>;
+        items.push(item);
+      }
+      setLocutores(items);
+    }).catch((error) => {
+      console.log("ERROR HERE")
+      if (error.response) {
+        console.log(error.response)
+        console.log(error.response.status)
+        console.log(error.response.headers)
+      }
+    })
+  }, []);
   
   return (
     <Fragment>
       <Menu home={false}/>
-      {getLocutores()}
+        {locutores}
     </Fragment>
   );
 }
