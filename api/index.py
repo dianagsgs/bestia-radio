289900f774d61365861f200008bc07f8071f7858
dianagsgs <deiaeneade@gmail.com>
2023-11-03 16:59:37 -0600

    start using sqlite

--- api/index.py
+++ api/index.py
@@ -1,13 +1,52 @@
 from flask import Flask, request, Response
 from flask_cors import CORS
-
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql import text
+from sqlalchemy import Integer, String
+from sqlalchemy.orm import Mapped, mapped_column
 import json
+import sys
 
 app = Flask(__name__)
 CORS(app)
 
+db = SQLAlchemy()
+db_name = 'bestia_radio.db'
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + db_name
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+db.init_app(app)
+
+# MODELS
+class Locutor(db.Model):
+    ID: Mapped[int] = mapped_column(Integer, primary_key=True)
+    nombre: Mapped[str] = mapped_column(String)
+    foto: Mapped[str] = mapped_column(String)
+    bio: Mapped[str] = mapped_column(String)
+    instagram: Mapped[str] = mapped_column(String)
+
+with app.app_context():
+    db.create_all()
+
+# ROUTES
+
+# this route will test the database connection - and nothing more
+@app.route('/')
+def testdb():
+    try:
+        db.session.query(text('1')).from_statement(text('SELECT 1')).all()
+        return '<h1>It works.</h1>'
+    except Exception as e:
+        # e holds description of the error
+        error_text = "<p>The error:<br>" + str(e) + "</p>"
+        hed = '<h1>Something is broken.</h1>'
+        return hed + error_text
+
+# DATABASE ACTUAL ROUTES
 @app.route('/api/get_locutores', methods=["GET"])
 def get_locutores():
+    locutores = db.session.execute(db.select(Locutor).order_by(Locutor.nombre)).fetchall()
+    print(locutores[1], file=sys.stderr)
     personas = [
         {
             "id":"1",
@@ -65,4 +104,4 @@ def get_eventos():
     return resp
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
